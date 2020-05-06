27f553ea2a0053b2c2e314ceda8ab607c2adace5
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 10:23:50 -0300

    entendendo o Bitwise

---
+++ aulas/Bitwise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bitwise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // << Dobrar o valor 
+            // >> Diminui pela métade
+            int num=10;
+            num=num<<1;
+            Console.WriteLine("Dobro : "+num);
+            int num2=5>>1;
+            Console.WriteLine("Métade : "+num2);
+        }
+    }
+}

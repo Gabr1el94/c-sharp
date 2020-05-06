fc3557ee790d9283ea6aef0e509bb345d6b25561
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 11:16:51 -0300

    repetição 'goto' implementado

--- aulas/calculadora/Program.cs
+++ aulas/calculadora/Program.cs
@@ -7,17 +7,25 @@ namespace calculadora
         
         static void Main(String[] args){
             int[] numeros = new int[2];
+            char escolha;
+            string operador;
+            
+            inicio:
+            Console.Clear();
             for (int i = 0; i <= 1; i++)
             {
                 Console.WriteLine("Informe o seu "+(i+1)+"º valor:");
                 numeros[i] = Convert.ToInt32(Console.ReadLine()); 
             }
+            
+              
+
             Console.WriteLine("Informe o tipo de operador:");
             Console.Write(" '+' -> Adição \n"
             +" '-'-> Subtração \n "
             +" '*'-> Multiplicação \n "
             +" '/'-> Divisão \n ");
-            string operador = Console.ReadLine();
+            operador = char.Parse(Console.ReadLine());
             switch (operador)
             {
                 case "+":
@@ -35,6 +43,15 @@ namespace calculadora
                 default:
                     Console.WriteLine("Operador não foi encontrado! :(");
                     break;
+            }
+            Console.Write("Deseja realizar o cálculo novamente?[s/n]:");
+            escolha = char.Parse(Console.ReadLine());
+            if (escolha=='s' || escolha=='S')
+            {
+                goto inicio;
+            }else{
+                Console.Clear();
+                Console.WriteLine("Fim do Programa");
             }
         }
     }

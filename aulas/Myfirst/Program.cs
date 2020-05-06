a049a9702f8b75b8f08ae8208ffeb018f7084667
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 08:43:50 -0300

    variáveis e constantes

--- aulas/Myfirst/Program.cs
+++ aulas/Myfirst/Program.cs
@@ -6,7 +6,13 @@ namespace Myfirst
     {
         static void Main(string[] args)
         {
+            //Constante
+             const double pi = 3.14159;
+            //Variavel
+            string nome = "Gabriel";
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Nome:"+nome+"\n Pi:"+pi);
             //Capturando os dados 'args'
             if (!args[0].Equals("") && !args[0].Equals(null)){
                 Console.WriteLine(args[0]);

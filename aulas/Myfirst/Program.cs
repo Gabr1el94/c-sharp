2904188796156dd6fe0e659654884e96f3f7c74c
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 06:39:23 -0300

    introdução ao c#

---
+++ aulas/Myfirst/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Myfirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Capturando os dados 'args'
+            if (!args[0].Equals("") && !args[0].Equals(null)){
+                Console.WriteLine(args[0]);
+            }
+        }
+    }
+}

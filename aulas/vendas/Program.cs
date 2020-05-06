95db1a70068f09204550be203cce464b508c83a0
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 08:43:36 -0300

    formatação de consoles

---
+++ aulas/vendas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace vendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorCompra = 5.50;
+            double valorVenda;
+            double lucro=0.1;
+            string produto = "Pastel";
+
+            valorVenda = valorCompra+(valorCompra*lucro);
+
+            Console.WriteLine("Produto:{0,15}",produto);
+            Console.WriteLine("Vlr.Compra:{0,15:c}",valorCompra);
+            Console.WriteLine("Lucro:{0,15:p}",lucro.ToString("P",CultureInfo.InvariantCulture));
+            Console.WriteLine("Val.Venda:{0,15:c}",valorVenda);
+        }
+    }
+}

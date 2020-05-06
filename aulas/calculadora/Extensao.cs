39a5f0ffb25cf1a35e84243f946fc39016f54a7f
Gabr1el94 <bielsystem64@gmail.com>
2020-05-06 08:44:12 -0300

    calculadora e método extensão

---
+++ aulas/calculadora/Extensao.cs
@@ -0,0 +1,20 @@
+namespace calculadora
+{
+    public static class Extensao
+    {
+        public static double soma(int value, int value2){
+            return value+value2;
+        }
+
+        public static double subtracao( int value, int value2){
+            return value-value2;
+        }
+        public static double multiplicacao( int value, int value2){
+            return value*value2;
+        }
+
+        public static double divisao( int value, int value2){
+            return value/value2;
+        }
+    }
+}
---
+++ aulas/calculadora/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        
+        static void Main(String[] args){
+            int[] numeros = new int[2];
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.WriteLine("Informe o seu "+(i+1)+"º valor:");
+                numeros[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+            Console.WriteLine("Informe o tipo de operador:");
+            Console.Write(" '+' -> Adição \n"
+            +" '-'-> Subtração \n "
+            +" '*'-> Multiplicação \n "
+            +" '/'-> Divisão \n ");
+            string operador = Console.ReadLine();
+            switch (operador)
+            {
+                case "+":
+                    Console.WriteLine("Resultado:"+Extensao.soma(numeros[0],numeros[1]));
+                    break;
+                case "-":
+                    Console.WriteLine("Resultado:"+Extensao.subtracao(numeros[0],numeros[1]));
+                    break;
+                case "*":
+                    Console.WriteLine("Resultado:"+Extensao.multiplicacao(numeros[0],numeros[1]));
+                    break;
+                case "/":
+                    Console.WriteLine("Resultado:"+Extensao.divisao(numeros[0],numeros[1]));
+                    break;
+                default:
+                    Console.WriteLine("Operador não foi encontrado! :(");
+                    break;
+            }
+        }
+    }
+}

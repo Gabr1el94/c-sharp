d4796216727cf189a2c30f862f7f9a2d344598ca
Gabr1el94 <bielsystem64@gmail.com>
2020-05-14 12:17:57 -0300

    ViewData

--- project_mvc_zero/Controller/HomeController.cs
+++ project_mvc_zero/Controller/HomeController.cs
@@ -4,6 +4,9 @@ namespace project_mvc_zero.Controler
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string nome { get; set; }
+
         public IActionResult Index()
         {
             return View();
@@ -11,16 +14,21 @@ namespace project_mvc_zero.Controler
 
          public IActionResult pag1()
         {
+            // ViewBag.mensagem1=  "Esta é a mensagem para página 1";
+            // ViewBag.mensagem2=  "Bem-vindo a página";
             return View();
         }
 
          public IActionResult pag2()
         {
+            // ViewData["nome"]="Gabriel";
+            // ViewData["apelido"]="Soares";
             return View();
         }
 
          public IActionResult pag3()
         {
+            this.nome="Gabriel";
             return View();
         }
      
